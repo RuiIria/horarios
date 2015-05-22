662ed92442f5809c290df92ae55385ea9e186251
RuiIria <RuiIria@hotmail.com>
2015-05-22 12:09:17 +0100

    Modal de Login
    o formulário de login foi alterado para ficar centralizado, performar a
    ação localizada no ficheiro "doLogin" e devolver o valor da função
    "ValidarLogin".

--- modLogin.php
+++ modLogin.php
@@ -1,3 +1,16 @@
-		<div class="row">
-			<div class="large-4 large-centered columns">Test</div>
-		</div>
+<div class='row' style='position:relative; top:15%; display:block;' id="login">
+	<div class='large-5 large-centered panel columns'>
+		<form method='post' onsubmit="return ValidarLogin();" action="doLogin">
+			<fieldset>
+				<legend>Login</legend>
+				<label>
+					Nome<input type="text" placeholder="ex: Zé Manel">
+				</label>
+				<br>
+				<label>
+					Senha<input type="password">
+				</label>
+			</fieldset>
+		</form>
+	</div>
+</div>

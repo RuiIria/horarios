53e7ab806174efbc9abb2260ce4ef0882f856e2d
RuiIria <RuiIria@hotmail.com>
2015-06-03 12:32:18 +0100

    Modal Login
    Começamos pelos scripts para as funções utilizadas para o Login. A
    função "ValidarEmailL" começa por definir uma variavel de regex para
    definir os parametros do email. Depois, vai buscar o que foi introduzido
    no input de Login e confirma se corresponde aos parametros da expressão
    regular. Se corresponder, vai mostrar a div "divEmailCorretoL" que irá
    mostrar a imagem "correto" e retorna um valor "true". se não, mostra o
    "divEmailErradoL" que mostra a imagem "errado" e retorna um valor
    "false".
    Para validar a passe funciona da mesma maneira, mas a expressão regular
    é diferente, as divs são diferentes e o input a que se tem de buscar o
    valor tambem.  De seguida é o formulário, que quando for submetido, vai
    iniciar o codigo no ficheiro "doLogin.php" e vair retornar o valor da
    função "ValidarLogin". De seguida estão os inputs do formulário, que vão
    buscar as funções correspondetes quando um valor for inserido. As divs
    de errado vão mostrar uma mensagem quando se passar com o rato por cima
    da imagem que vai mostrar. No final estão dois botões. um para performar
    o Login, e outro para realizar a função "ApareceRegisto" que vai
    esconder o modal de Login e mostrar o modal de Registo.

--- modLogin.php
+++ modLogin.php
@@ -1,16 +1,66 @@
-<div class='row' style='position:relative; top:15%; display:block;' id="login">
+<script>
+		function ValidarEmailL(){
+			var reg = /[A-Za-z0-9._-]+@[a-z]+\.[a-z]{2,3}(\.[a-z]{2,3})?$/;
+			if(reg.test(document.getElementById("EmailL").value)== true){
+				document.getElementById('divEmailErradoL').style.display='none'; 
+				document.getElementById('divEmailCorretoL').style.display='block'; //Mostra email correto
+				return true;
+			}
+			else{
+				document.getElementById('divEmailErradoL').style.display='block';//mostra email errado
+				document.getElementById('divEmailCorretoL').style.display='none';
+				return false;
+			}
+		}
+		function ValidarPass(){
+			var reg = /((?=.*[0-9])(?=.*[A-Z]).{6,15})/;
+			if(reg.test(document.getElementById("pass").value)== true){
+				document.getElementById('divPassErrada').style.display='none';
+				document.getElementById('divPassCorreta').style.display='block';//mostra pass certa
+				return true;
+				}
+			else{
+				document.getElementById('divPassErrada').style.display='block';//mostra pass errada
+				document.getElementById('divPassCorreta').style.display='none';
+				return false;
+			}
+		}
+</script>
+<div class='row' style='position:relative; top:15%; display:block;' id="Login">
 	<div class='large-5 large-centered panel columns'>
-		<form method='post' onsubmit="return ValidarLogin();" action="doLogin">
+		<form method='post' action="doLogin.php" onsubmit="return ValidarLogin();" >
 			<fieldset>
-				<legend>Login</legend>
+				<legend><b>LOGIN</b></legend>
 				<label>
-					Nome<input type="text" placeholder="ex: Zé Manel">
+					Email:<input type="text" onkeyup='ValidarEmailL()' onchange='ValidarEmailL()' name="EmailL" id="EmailL" required />
 				</label>
+					<div id="divEmailErradoL" style='display:none; position: absolute; right: 48px; top: 108px;'>
+						<span data-tooltip aria-haspopup="true" class="has-tip" title="Nome inválido">
+							<img src="./img/errado.png" height="20px" width="20px">
+						</span>
+					</div>
+					<div id="divEmailCorretoL" style='display:none; position: absolute; right: 48px; top: 109px;'>
+						<img src="./img/correto.png" height="20px" width="20px">
+					</div>
 				<br>
 				<label>
-					Senha<input type="password">
+					Senha:<input type="password" onkeyup='ValidarPass()' onchange='ValidarPass()' name="pass" id="pass" required />
 				</label>
+					<div id="divPassErrada" style='display:none; position: absolute; right: 48px; bottom: 147px;'>
+						<span data-tooltip aria-haspopup="true" class="has-tip" title="Password inválida">
+							<img src="./img/errado.png" height="20px" width="20px">
+						</span>
+					</div>
+					<div id="divPassCorreta" style='display:none; position: absolute; right: 48px; bottom: 149px;'>
+						<img src="./img/correto.png" height="20px" width="20px">
+					</div>
 			</fieldset>
+			<div class='row'>
+				<div class='large-9 large-centered columns'>
+					<input type='submit' class='small button' value='ENTRAR' />
+					<a onclick="ApareceRegisto()" class="small button"> CRIAR CONTA </a>
+				</div>
+			</div>
 		</form>
 	</div>
 </div>

e943149f8de7da84345b382233c09505ec3b7c0b
RuiIria <RuiIria@hotmail.com>
2015-06-03 12:45:32 +0100

    scripts.php
    o ficheiro "scripts.php" vai ligar aos ficheiros do foundation. De
    seguida, está a função "ApareceRegisto" e "ApareceLogin" cujas funções
    foram anteriormente explicadas. A função "ValidarLogin" vai confirmar se
    ambas as funções "ValidarPass" e "ValidarEmailL" retornam valor "true"
    para depois tambem retornar um valor "true". se não, vai avisar que um
    dos campos está inválido. A função "ValidarRegisto" vai confirmar se as
    funções "ValidarNomeR", "ValidarPassR", "ValidarEmail" e "ValidarCPass"
    retornam todas valor "true" para depois retornar um valor "true". mas se
    não retornarem todos valor "true" vai avisar que um dos campos
    encontra-se inválido, e retorna uma valor "false"

---
+++ scripts.php
@@ -0,0 +1,40 @@
+
+		<script src="foundation/js/vendor/jquery.js"></script>
+		<script src="foundation/js/vendor/modernizr.js"></script>
+        <script src="foundation/js/foundation.min.js"></script>
+		<script src="foundation/js/foundation/foundation.js"></script>
+		<script src="foundation/js/foundation/foundation.topbar.js"></script>
+        <script>
+          $(document).foundation();
+        </script>
+		<script>
+		function ApareceRegisto(){
+		document.getElementById('Registar').style.display='block';
+		document.getElementById('Login').style.display='none';
+		}
+		function ApareceLogin(){
+		document.getElementById('Login').style.display='block';
+		document.getElementById('Registar').style.display='none';
+		}
+		function ValidarLogin(){
+		if(ValidarPass() && ValidarEmailL()){			
+			return true;
+		}
+		else{			
+			alert("Um dos campos encontra-se invalido! Por favor preecha-os corretamente.");
+			return false;
+			}
+		}
+		function ValidarRegisto(){
+			
+			alert('oi');
+			
+		if(ValidarNomeR() && ValidarPassR() && ValidarEmail() && ValidarCPass()){			
+			return true;
+		}
+		else{			
+			alert("Um dos campos encontra-se invalido! Por favor preecha-os corretamente.");
+			return false;
+			}
+		}
+		</script>

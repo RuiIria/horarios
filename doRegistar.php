18260d62555dc246ace8c42ced952eb57307c5f4
RuiIria <RuiIria@hotmail.com>
2015-06-03 12:16:38 +0100

    doRegistar
    doRegistar é a ação que vai executra quando o formulário de registo for
    submetido. primeiro, vai confirmar se o email inserido não existe na
    base de dados. Depois cria variaveis com os valores inseridos no
    formulário. Depois, vai incluir o ficheiro "DataAccess.php" no codigo,
    que será a variavel "da". De seguida vai performar a função
    "EmailJaExiste" que encontra-se no ficheiro DataAccess. Se encontrar o
    emial dentro da base de dados, vai alertar ao utilizador que esse email
    ja tem uma conta, se não, vai introduzir os valores de da conta dentro
    da base de dados.

---
+++ doRegistar.php
@@ -0,0 +1,19 @@
+﻿<?php
+	if (isset($_POST['email'])){
+		//tentar efetuar o registo
+		$nome = $_POST['nomeR'];
+		$Email = $_POST['email'];
+		$pwd = md5($_POST['passR']);		//md5 serve para encriptar a password
+		include_once('DataAccess.php');
+		$da = new DataAccess();
+		$res = $da->EmailJaExiste($Email);		
+		//Se a variável $res tiver resultados, significa que existe ja uma conta com o msm email.
+		if ( !$res ) {
+			echo "<script>alert('Esse email já se encontra registado.');</script>";
+		}else{
+			$da->inserirUtilizador($nome, $Email, $pwd);
+			echo "<script>alert('Registo efetuado com sucesso');
+				window.location='index.php';</script>";
+		}
+	}
+?>

8ba4f6486eed48c9e90815178db9cb842a9287e0
RuiIria <RuiIria@hotmail.com>
2015-06-17 09:54:22 +0100

    Topbar.php
    foi colocado codigo dentro do ficheiro "Topbar.php". começamos por criar
    uma div com a classe fixada, e depois usamos a tag "nav" para criar um
    menu. Nesse menu, metemos os botões para is para a pagina das definições
    da conta, realizar o log-out, ver horarios já criados ou criar um novo
    horário.

---
+++ Topbar.php
@@ -0,0 +1,15 @@
+﻿<div class="fixed">
+	<nav class="top-bar" data-topbar role="navigation">
+		<section class="top-bar-section">
+			<ul class="right">
+				<li><a href="definicoesconta.php">Definições da conta</a></li>
+				<li><a href='logout.php'>Sair</a></li>
+			</ul>
+			
+			<ul class="left">
+				<li><a href="modVerHorario.php">Ver Horários</a></li>
+				<li><a href="modCriarHorario.php">Criar Horários</a></li>
+			</ul>
+		</section>
+	</nav>
+ </div>
